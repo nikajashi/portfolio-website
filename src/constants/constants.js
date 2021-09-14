85f4a4d3ad64c759b40ea48dd5a05428538710d1
Nika <jashin@tcd.ie>
2021-09-14 12:16:49 +0100

    Iniatial commit

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,11 +1,11 @@
 export const projects = [
   {
-    title: 'MERN Memories',
+    title: 'University',
     description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
       image: '/images/1.png',
       tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    source: 'https://github.com/nikajashi/university',
+    visit: 'https://github.com/nikajashi/university',
     id: 0,
   },
   {
